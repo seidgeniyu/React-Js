607572daea990f4410d3ea69f930f1ab88ae8564
Seid Geniyu <seyageniyu@gmail.com>
2024-05-03 14:22:38 +0300

    React js with tailwind css

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,9 @@
+import './App.css'
 import Message from "./Message";
 function App(){
-   return <div><Message /></div>
+   return <div className='bg-blue-600 text-xl'><Message />
+   <p>React js with tailwind</p>
+   </div>
+   
 }
 export default App;
--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,6 +1,14 @@
+
 //PascaleCasing
 function Message(){
     //jsx
-return <h1>Hello World</h1>;
+const name = 'seya'; 
+if(name){
+    return <h1 className="bg-red-700">Hello {name}</h1>;
+}
+else{
+    return <h1>Hello World</h1>;
+}
+
 }
 export default Message;
